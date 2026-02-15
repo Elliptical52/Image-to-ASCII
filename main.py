1f6edbb813475be13658e557281e558e732bc8ae
Tom Martin <tommartin2085@gmail.com>
2026-02-15 15:11:15 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+import numpy as np
+from PIL import Image
+
+width = 64
+height = 64
+input_path = "example.png"
+
+
+characters = " -':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
+
+RESET = "\x1b[0m"
+
+image = Image.open(input_path).convert("RGBA")
+image = image.resize((width, height))
+
+image_array = np.array(image)
+
+
+output_string = ''
+
+for y in range(height):
+    for x in range(width):
+        r, g, b, a = image_array[y, x]
+
+        color_code = f'\x1b[38;2;{r};{g};{b}m'
+        brightness = (0.2126 * r + 0.7152 * g + 0.0722 * b) / 255
+        index = int(brightness * (len(characters) - 1))
+        character = characters[index]
+        output_string += color_code + character * 2
+    output_string += '\n'
+
+
+
+print(output_string)
